5a0c02e2e30b33b0c5744978be9cefa48b50138a
Tonny Petersen <tsp@person.dk>
2012-02-06 22:41:58 +0100

    fix: win32 compiler errors

--- lib/platform/windows/os-types.h
+++ lib/platform/windows/os-types.h
@@ -68,7 +68,7 @@ typedef unsigned __int64 uint64_t;
 #ifdef  _WIN64
 typedef __int64    ssize_t;
 #else
-typedef _W64 int   ssize_t;
+//typedef _W64 int   ssize_t;
 #endif
 #define _SSIZE_T_DEFINED
 #endif
